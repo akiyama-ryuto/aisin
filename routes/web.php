92e807495bf399c9aba0d3369694f21a1909ac5e
rel20251224_03\r.akiyama <akiyama.ryuto@relation2012.com>
2026-04-24 16:29:50 +0900

    railwayの自動ビルドが正常に行われていることをチェック

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/version-check', function () {
+    return [
+        'laravel' => app()->version(),
+        'php' => phpversion(),
+    ];
+});
