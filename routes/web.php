04c567ce61dbb5d51cc46c82fcbd72801c998dbd
rel20251224_03\r.akiyama <akiyama.ryuto@relation2012.com>
2026-04-24 16:31:30 +0900

    Revert "railwayの自動ビルドが正常に行われていることをチェック"
    This reverts commit 92e807495bf399c9aba0d3369694f21a1909ac5e.

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/version-check', function () {
-    return [
-        'laravel' => app()->version(),
-        'php' => phpversion(),
-    ];
-});
